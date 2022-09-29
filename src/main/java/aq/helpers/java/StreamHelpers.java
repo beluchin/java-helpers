8aefc1e8ba495302190cec7c836bf097a4dee885
Abel Quiros <beluchin@gmail.com>
2022-09-29 11:19:27 +0100

    fix javadoc

--- src/main/java/aq/helpers/java/StreamHelpers.java
+++ src/main/java/aq/helpers/java/StreamHelpers.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 @UtilityClass
 public final class StreamHelpers {
     /**
-     * @see <a ref="https://clojuredocs.org/clojure.core/repeatedly">https://clojuredocs.org/clojure.core/repeatedly</a>
+     * @see <a href="https://clojuredocs.org/clojure.core/repeatedly">https://clojuredocs.org/clojure.core/repeatedly</a>
      */
     public static <T> Stream<T> repeatedly(Supplier<T> supplier, int times) {
         return Stream.generate(supplier).limit(times);
